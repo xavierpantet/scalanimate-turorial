9a20c0c9c93889cd067101a57e4afa7f92197656
Xavier Pantet <xavpantet@gmail.com>
2017-12-11 11:30:13 +0100

    Few changes and wrote beginning of shapes + demo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,17 @@
 package Main
 
+import scalanimate.Canvas
+import org.scalajs.dom.document
+import org.scalajs.dom.raw.HTMLCanvasElement
+
+import scalanimate.shapes.mutable.{Circle, Oval, Polygon, Rectangle}
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hey!")
+    val c = Canvas(document.getElementById("shapesCanvas").asInstanceOf[HTMLCanvasElement])
+    Circle(100, 50, 20)(c)
+    Rectangle(80, 130, 50, 120)(c)
+    Polygon(200, 80, 30, 5)(c)
+    Oval(150, 20, 60, 40)(c)
   }
 }
